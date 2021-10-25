3aee4f2cdbb971182ae0618a37092579a7f03f87
izadoramatias <contato.izadoramatias@gmail.com>
2021-10-25 01:32:52 -0300

    last commit

--- scripts/script.js
+++ scripts/script.js
@@ -31,8 +31,54 @@ function getGitHubUserInfos(){
 
 }
 
+function addSpanFaixaWhenWidth420(){
+  const span = document.querySelector("#faixa");
+  const elementToRemove = document.querySelector('img.imgFaixa')
+  const widthScreen = window.screen.width;
 
+  if (widthScreen <= 460){
+    elementToRemove.parentNode.removeChild(elementToRemove)
+  }else{
+    elementToRemove.parentNode.prepend(elementToRemove)
+  }
+
+}
+
+VanillaTilt.init(document.querySelector("main"), {
+		max: 25,
+		speed: 400,
+    glare: true
+	});
 
 // chamadas das funções
 changeSocialMediaLinks();
-getGitHubUserInfos()
+getGitHubUserInfos();
+addSpanFaixaWhenWidth420()
+
+
+// background squares
+const ulSquares = document.querySelector("ul.squares");
+
+for (let i = 0; i < 11; i++){
+  const li = document.createElement("li");
+
+  const random = (min, max) => Math.random() * (max - min) + min;
+
+  const size = Math.floor(random(10, 120));
+  const position = random(1, 99);
+  const delay = random(5, 0.1);
+  const duration = random(24, 12);
+
+
+  li.style.width = `${size}px`;
+  li.style.height = `${size}px`;
+  li.style.bottom = `-${size}px`;
+  
+  li.style.left = `${position}%`;
+  
+  li.style.animationDelay = `${delay}s`;
+  li.style.animationDuration = `${duration}s`;
+  li.style.animationTimingFunction = `cubic-bezier(${Math.random()},${Math.random()},${Math.random()},${Math.random()})`;
+
+  ulSquares.appendChild(li);
+}
